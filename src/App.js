dd079f0dbe286abc13e480e7614abcd06c4c82dc
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2018-09-25 09:07:47 -0500

    Files changed for new structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Routes from './Routes';
+import NavBar from './components/NavBar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">El weeks me la pela :v</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <NavBar />
+        <br />
+        <div className="container-fluid">
+          <Routes />
+          <br />
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "./redux/configureStore";
+import "./styles/toastr.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export const store = configureStore();
+
+const RouterService = () => (
+    <BrowserRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </BrowserRouter>
+);
+
+ReactDOM.render(<RouterService />, document.getElementById('root'));
 registerServiceWorker();
