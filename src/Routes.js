281adeabc3857f18be2e383669056137fdbcb69c
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2018-09-27 10:24:20 -0500

    Navigation actualizada

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { Route, Switch } from "react-router-dom";
 import JoinForm from './components/JoinForm';
+import ListOfTeams from './components/ListOfTeams';
+import MemberSignIn from './components/MemberSignIn/MemberSignIn';
+import ProfileView from './components/Profile/ProfileView';
 
 class Routes extends Component {
     render() {
         return (
             <Switch>
+                <Route exact path="/registro/miembro/:id" component={MemberSignIn} />
                 <Route exact path="/" component={JoinForm} />
+                <Route exact path="/lista" component={ListOfTeams} />
+                <Route exact path="/perfil" component={ProfileView} />
             </Switch>
         );
     }
