7fbcf9dd495a99890557f37b58ee334d10b60663
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2018-09-27 23:08:09 -0500

    UploadImage actualizado

--- src/components/UploadImage/UploadImage.jsx
+++ src/components/UploadImage/UploadImage.jsx
@@ -9,9 +9,9 @@ class UploadImage extends Component {
                         <input type="file" className="custom-file-input" name="image" id="image" onChange={this.props.handleUpload.bind(this)} />
                         <label className="custom-file-label" htmlFor="image">{this.props.fileLabel}</label>
                 </div>
-                <div class="progress">
+                <div className="progress">
                     <br/>
-                    <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" style={{width: this.props.uploadProgress.toString()+"%"}} aria-valuenow={this.props.uploadProgress} aria-valuemin="0" aria-valuemax="100"></div>
+                    <div className="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" style={{width: this.props.uploadProgress.toString()+"%"}} aria-valuenow={this.props.uploadProgress} aria-valuemin="0" aria-valuemax="100"></div>
                     <br/>
                 </div>
             </div>
